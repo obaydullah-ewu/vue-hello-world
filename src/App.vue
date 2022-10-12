a97cb1154e3f0e0ae692b2ae4ba4f790749c3251
Md Obaydullah <mdobaydullahornob@gmail.com>
2022-10-13 01:37:00 +0600

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MyBaby msg="hello" />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import MyBaby from './components/MyBaby'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    MyBaby
   }
 }
 </script>
---
+++ src/components/MyBaby.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h5>Hi I am MD Obaydullah</h5>
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'MyBaby',
+    props: {
+      'msg': String
+    }
+  }
+</script>
